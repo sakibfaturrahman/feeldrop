b656bb4f0ad3edd46164024f696bc933533340cd
Sakib Faturrahman <sakibfaturrahman92@gmail.com>
2025-06-04 11:56:15 +0700

    Fix: sesuaikan server.js agar jalan di Vercel

--- server.js
+++ server.js
@@ -30,3 +30,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server jalan di http://localhost:${PORT}`);
 });
+
+module.exports = app; // Export app untuk testing
